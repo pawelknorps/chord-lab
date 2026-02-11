1c93ad83bc5c75ee757d0da6cd121393d84fed6c
pawelknorps <56291219+pawelknorps@users.noreply.github.com>
2026-02-11 17:21:37 +0100

    chore(audio): optimize mic constraints for instrument pitch detection

--- src/hooks/usePitchTracker.ts
+++ src/hooks/usePitchTracker.ts
@@ -30,9 +30,9 @@ export function usePitchTracker(isReady: boolean): React.MutableRefObject<PitchT
 
         const stream = await navigator.mediaDevices.getUserMedia({
           audio: {
-            echoCancellation: true,
-            noiseSuppression: true,
-            autoGainControl: true,
+            echoCancellation: false,
+            noiseSuppression: false,
+            autoGainControl: false,
           },
           video: false,
         });
